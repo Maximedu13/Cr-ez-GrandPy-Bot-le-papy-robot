b17a96048f1de2f27bd3a3caa92ea07e8e20bdad
Maximedu13 <maxime.j.pro@free.fr>
2019-05-07 14:43:12 +0200

    google maps api

--- GrandPyBot/static/bootstrap/index.js
+++ GrandPyBot/static/bootstrap/index.js
@@ -1,1 +1,32 @@
-//$(function() { $(':submit').on('click', function() { var usr_query = document.getElementById('search').value; if (usr_query !== "") { document.write("Vous avez saisi :" + usr_query); loading(); ajaxPost(usr_query, data_treat); } }); });
+//$(function() { $(':submit').on('click', function() { var usr_query = document.getElementById('search').value; if (usr_query !== "") { document.write("Vous avez saisi :" + usr_query); loading(); ajaxPost(usr_query, data_treat); } }); })
+var lat;
+var long;
+
+function initMap(lat, long, loc) {
+    var city = {
+        lat: lat,
+        lng: long
+    };
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 10,
+            center: city,
+        });
+    var contentString = 
+    '<div id="content">'+ '<div id="siteNotice">'+ '</div>'+ '<h1 id="firstHeading" class="firstHeading">'+ loc +'</h1>' + '</div>'
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+        });
+    var marker = new google.maps.Marker({
+        position: city,
+        map: map,
+        title: "la ville"
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+}
+function display_map(){
+    $("#map").show();
+}
--- GrandPyBot/views.py
+++ GrandPyBot/views.py
@@ -42,9 +42,9 @@ def search():
     for key,value in gm.get_position(the_question).items():
         list_values.append(value)
     gmaps.configure(api_key=gm.key_api)
-
     print(the_question)
     print(google_maps_question)
+    print(list_values[1], list_values[2])
     print("https://www.google.com/maps/place/" + google_maps_question + str('/') +  str(list_values[1]) + str(",") + str(list_values[2])) 
     try:
         for key, value in wiki.get_wiki_result(the_question).items():
@@ -54,9 +54,9 @@ def search():
         + "<em>" + (random.choice(msg_fails)) + question + "</em>"
    
     if value:
-        return "<b>" + "Vous m‘avez posé comme question : " + request.form['search'] + "</b>" + "<br/>"  \
-        + "<em>" + (random.choice(GrdPy_msg)) + "</em>" + value +  "<br/>" + \
-        '<div style="width: 700px;position: relative;"><iframe width="700" height="440" src="https://www.google.com/maps/place/" + google_maps_question + "/" +  str(list_values[1]) + "</div>"'
+        return "<script>initMap(" + str(list_values[1]) + ',' + str(list_values[2]) + ',' + "'" + google_maps_question + "'" +");display_map(); </script>" + "<b>" + "Vous m‘avez posé comme question : " + request.form['search'] + "</b>" + "<br/>" \
+        + "<em>" + (random.choice(GrdPy_msg)) + "</em>" + value +  "<br/>" 
+        
     else:
         return "<b>" + "Vous m‘avez posé comme question : " + request.form['search'] + "</b>" + "<br/>"  \
             + "<em>" + (random.choice(msg_fails)) + question + "</em>" + \
