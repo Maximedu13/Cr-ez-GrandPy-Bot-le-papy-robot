0c0f1ae58c466b9acc31605adfb92ca39cd11152
Maximedu13 <maxime.j.pro@free.fr>
2019-10-09 14:50:10 +0200

    add temperature

--- GrandPyBot/apis.py
+++ GrandPyBot/apis.py
@@ -1,5 +1,7 @@
 """apis"""
 import wikipediaapi
+import requests
+import json
 from requests import get
 
 WIKI_WIKI = wikipediaapi.Wikipedia('fr')
@@ -26,7 +28,7 @@ class Wiki():
 class GoogleMaps():
     """ google maps api """
     def __init__(self):
-        self.key_api = ""
+        self.key_api = "AIzaSyDEexokSoTAXo8lWdcuCF1ia1cw8m2fcRk"
 
     def get_position(self, question):
         """ main method """
@@ -54,3 +56,15 @@ class GoogleMaps():
 
         except:
             return "no result"
+
+class Weather():
+    """ https://openweathermap.org/api """
+    def __init__(self):
+        self.key_api = "3cf63bac22ea72262bb10f739247a998"
+
+    def get_the_weather(self, question):
+        req = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + question \
+        + "&appid=" + self.key_api)
+        result = json.loads(req.text)
+        return result["weather"][0]["main"], round(result["main"]["temp"] - 273.15)
+        
--- GrandPyBot/views.py
+++ GrandPyBot/views.py
@@ -1,10 +1,25 @@
 """views.py"""
 import random
 from flask import Flask, request, render_template
-from GrandPyBot.apis import Wiki, GoogleMaps
+from flask_mail import Mail, Message
+from GrandPyBot.apis import Wiki, GoogleMaps, Weather
 from GrandPyBot.messages import Message
+import os
 
 app = Flask(__name__)
+mail = Mail(app)
+mail_settings = {
+    "MAIL_SERVER": 'localhost',
+    "MAIL_PORT": 25,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": None, #os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": None #os.environ['EMAIL_PASSWORD']
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+print(mail)
 
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
@@ -13,16 +28,23 @@ app.config.from_object('config')
 @app.route('/search', methods=['POST'])
 def search():
     """launch a search"""
+    # instanciation
+    wiki, message, weather, g_m = Wiki(), Message(), Weather(), GoogleMaps()
+
     question = request.form['search']
-    wiki = Wiki()
-    message = Message()
+   
     grd_py_msg = message.grandpy_msg
     msg_fails = message.grandpy_msg_fails
     the_question = message.parse_msg(question)
-    g_m = GoogleMaps()
+    
     g_m.get_position(the_question)
     list_values = []
-
+    
+    the_weather, degrees = weather.get_the_weather(the_question)
+    print(the_weather)
+    print(degrees)
+    this_image = "../static/images/" + the_weather + ".svg"
+    print(this_image)
     # If google maps api returns something not null
     try:
         for key, value in g_m.get_position(the_question).items():
@@ -49,8 +71,10 @@ def search():
     # If it returns a right information from media wiki and google maps
     if value and g_m.get_position(the_question) != "no result":
         return "<script>initMap(" + str(list_values[1]) + ',' + str(list_values[2]) + ',' + \
-            "'" + the_question + "'" +");display_map(); </script>" + "<b>" + \
-            "Vous m‘avez posé comme question : " + request.form['search'] + "</b>" + "<br/>" \
+            "'" + the_question + "'" +");display_map(); </script>" + "<b>"  + \
+            "Vous m‘avez posé comme question : " + request.form['search'] + "</b>" + "<br/>" + \
+            "<img src="+ this_image + "/>" + \
+            "<br/>" + "<b>" + str(degrees) + "</b>" + "°C" +"<br/>" \
             + "<em>" + (random.choice(grd_py_msg)) + "</em>" + value +  "<br/>"
 
     # If it returns no information from media wiki, only from google maps (address)
