93bfca170c225c6604b725271c49c5773d121175
Maximedu13 <maxime.j.pro@free.fr>
2019-11-13 11:06:15 +0100

    fix bug path heroku logs7

--- GrandPyBot/views.py
+++ GrandPyBot/views.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 app.config.update(dict(
         MAIL_SERVER = 'smtp.gmail.com',
-        MAIL_PORT = 587,
+        MAIL_PORT = 25,
         MAIL_USE_TLS = True,
         MAIL_USE_SSL = False,
         MAIL_USERNAME = 'maxim95470@gmail.com',
