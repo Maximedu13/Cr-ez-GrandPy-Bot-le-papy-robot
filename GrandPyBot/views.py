6d5201a55ce850ae14cfb698bfd744cb6e509861
Maximedu13 <maxime.j.pro@free.fr>
2019-11-13 10:35:40 +0100

    fix bug path heroku logs2

--- GrandPyBot/views.py
+++ GrandPyBot/views.py
@@ -87,6 +87,7 @@ def search():
         return "<b>" + "Vous m‘avez posé comme question : " + request.form['search'] + \
             "</b>" + "<br/>" + "<em>" + (random.choice(msg_fails)) + question + "</em>"
 
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     """index"""
